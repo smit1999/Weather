f6c3711609343c7e71b4f81b5ef779d1efd56851
smit1999 <35296246+smit1999@users.noreply.github.com>
2019-03-09 19:45:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,25 @@ app.debug = True
 
 @app.route('/')
 def index():
-   city = 'Mumbai'
-   if request.method == 'GET':
-      city = request.args.get('city')
-   #print(city)
-   req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
-   
-   print(req)
-   return render_template('index.html')
 
+        if request.method == 'GET':
+         city = request.args.get('city')
+
+        city = 'mumbai'
+        #print(city)
+        req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
+   
+
+
+        data={
+         'city':city,
+         'temperature': req['main']['temp'],
+         'desc': req['weather'][0]['description'],
+         'icon': req['weather'][0]['icon'],
+        }
+
+        print(data)
+
+        return render_template('index.html', data=data)
 if __name__ == '__main__':
    app.run()
