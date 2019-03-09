0b4af07abafd2e04a65293017b6bc65fa55e92f9
Zenil Haria <zenil.haria@somaiya.edu>
2019-03-09 17:29:53 +0530

    JSon Parsing added along with API request

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def hello_world():
+def index():
+   city = 'Mumbai'
+   if request.method == 'GET':
+      city = request.args.get('city')
+   #print(city)
+   req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
+   
+   print(req)
    return render_template('index.html')
 
 if __name__ == '__main__':
